8d69be783f5208b5437025aab31f8790a8014658
Lilian <chinweiloani@yahoo.com>
2022-11-10 15:06:06 +0100

    first commit

---
+++ backend/controllers/comment.js
@@ -0,0 +1,80 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
+
+export const getComments = (req, res) => {
+  /* const token = req.cookies.accessToken;
+  
+  if (!token) return res.status(401).json("Pas connecté!");
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token n'est pas valide!");
+   */
+  
+  const q = `SELECT c.*, u.id AS userId, username, profilePicture FROM comments AS c JOIN users AS u ON (u.id = c.userId)
+    WHERE c.postId = ? ORDER BY c.createdAt DESC
+    `;
+
+  db.query(q, [req.query.postId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data); 
+ /*  }); */
+  });
+};
+
+export const addComment = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Pas connecté!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token n'est pas valide!");
+
+    const q = "INSERT INTO comments(`desc`, `createdAt`, `userId`, `postId`) VALUES (?)";
+    const values = [
+      req.body.desc,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+      req.body.postId
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Le commentaire a été créé.");
+    });
+  });
+};
+
+/* export const deleteComment = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Non authentifié!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token n'est pas valide!");
+
+    const commentId = req.params.id;
+    const q = "DELETE FROM comments WHERE `id` = ? AND `userId` = ?";
+
+    db.query(q, [commentId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.affectedRows > 0) return res.json("Le commentaire a été supprimé!");
+      return res.status(403).json("Vous ne pouvez supprimer que votre commentaire!");
+    });
+  });
+}; */
+
+export const deleteComment = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+    const commentId = req.params.id;
+    const q =
+      "DELETE FROM comments WHERE `id`=? AND `userId` = ?";
+
+    db.query(q, [commentId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if(data.affectedRows>0) return res.status(200).json("comment has been deleted.");
+      return res.status(403).json("You can delete only your comment")
+    });
+  });
+};
