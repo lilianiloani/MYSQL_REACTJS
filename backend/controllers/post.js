ea1bbb9f2ac6b5f6d6e438567d4f4c501d4bf555
Lilian <chinweiloani@yahoo.com>
2022-11-22 13:46:44 +0100

    ...

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -49,61 +49,56 @@ export const deletePost = async (req, res) => {
   let post_id = req.params.id;
   let query = "SELECT * FROM posts WHERE `id` = ?";
 
-  db.query(query, [post_id], (error, result)=>{
-    if(error) return result.status(404).json('Post not found');
+  db.query(query, [post_id], (error, result) => {
+    if (error) return result.status(404).json("Post not found");
     let post = result[0];
 
     let userId = req.params.user_id;
-    let isAdmin = 0
+    let isAdmin = 0;
     let user = "SELECT * FROM users WHERE `id` = ?";
-    db.query(user, [userId], (error,result)=>{
-      if(error) return result.status(404).json('user not found'); 
+    db.query(user, [userId], (error, result) => {
+      if (error) return result.status(404).json("user not found");
       isAdmin = result[0].isAdmin;
-      console.log("user:>> ", result)
     });
-    
-    if(req.params.user_id == post.userId || isAdmin === 1){
 
-      const q = "DELETE FROM posts WHERE `id`=?";  
-      db.query(q, [post_id], (err, data) => { 
+    if (req.params.user_id == post.userId || isAdmin === 1) {
+      const q = "DELETE FROM posts WHERE `id`=?";
+      db.query(q, [post_id], (err, data) => {
         if (err) return res.status(500).json(err);
         if (data.affectedRows > 0)
           return res.status(200).json("Post has been deleted.");
-      
       });
-    }
-    else{
-      return res.status(401).json("You do not have access to this resource.")
+    } else {
+      return res.status(401).json("You do not have access to this resource.");
     }
   });
-
 };
 
 export const updatePost = (req, res) => {
-
   let post_id = req.params.id;
   let query = "SELECT * FROM posts WHERE `id` = ?";
 
-  db.query(query, [post_id], (error, result)=>{
-    if(error) return result.status(404).json('Post not found');
+  db.query(query, [post_id], (error, result) => {
+    if (error) return result.status(404).json("Post not found");
     let post = result[0];
 
     let userId = req.params.user_id;
-    let isAdmin = 0
+    let isAdmin = 0;
     let user = "SELECT * FROM users WHERE `id` = ?";
-    db.query(user, [userId], (error,result)=>{
-      if(error) return result.status(404).json('user not found'); 
+    db.query(user, [userId], (error, result) => {
+      if (error) return result.status(404).json("user not found");
       isAdmin = result[0].isAdmin;
-      console.log("user:>> ", result)
     });
-    
-    if(req.params.user_id == post.userId || req.user.isAdmin === 1){
+
+    if (req.params.user_id == post.userId || req.user.isAdmin === 1) {
       let file = req.file;
 
       const fileName = `${v4()}.${file.originalname.split(".").splice(-1)[0]}`;
 
       const url = path.normalize(
-        `${path.resolve(path.dirname(""))}../../frontend/public/upload/${fileName}`
+        `${path.resolve(
+          path.dirname("")
+        )}../../frontend/public/upload/${fileName}`
       );
 
       fs.writeFileSync(url, file.buffer, { flag: "w" });
@@ -113,10 +108,8 @@ export const updatePost = (req, res) => {
         if (err) res.status(500).json(err);
         return res.status(200).json(data);
       });
-    }
-    else{
-      return res.status(401).json("You do not have access to this resource.")
+    } else {
+      return res.status(401).json("You do not have access to this resource.");
     }
   });
-
 };
--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -3,86 +3,81 @@ import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlin
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
-import { useState, useContext, useEffect} from "react";
+import { useState, useContext, useEffect } from "react";
 import moment from "moment";
 import { makeRequest } from "../../axios";
 import { AuthContext } from "../../context/authContext";
 import ProfileImg from "../../img/avatarP.webp";
 import PostUpdate from "../../components/postUpdate/PostUpdate";
 
-
-
-export default function Post({ post,handlePostDelete  }) {
+export default function Post({ post, handlePostDelete }) {
   const [commentOpen, setCommentOpen] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
+  //const [menuOpen, setMenuOpen] = useState(false);
   const [updateOpen, setUpdateOpen] = useState(false);
-   const [likes, setLikes] = useState([]); 
+  const [likes, setLikes] = useState([]);
   const [user, setUser] = useState({});
-   const { currentUser } = useContext(AuthContext); 
-   const [likesLoading, setLikesLoading] = useState(false);
-  
-   const getLikes = async() =>{
-    
-    setLikesLoading(value=>!value);
-    try{
-      let queryLikes = await makeRequest.get(`/likes/${post.id}`, {headers:{
-        Authorization:`Bearer ${currentUser.token}`,
-      }});
-       setLikes(queryLikes.data); 
-      setLikesLoading(value=>!value);
-    }catch(e){
-  
-    }
-    
-   }
-  
-  const handleLike = async() => {
-  
-    const token = JSON.parse(localStorage.getItem('user')).token;
-    await makeRequest.put(`/likes`, {postId: post.id}, {
-      headers:{
-        Authorization: `Bearer ${token}` 
-      },
-    });
-  
+  const { currentUser } = useContext(AuthContext);
+  const [likesLoading, setLikesLoading] = useState(false);
+
+  const getLikes = async () => {
+    setLikesLoading((value) => !value);
+    try {
+      let queryLikes = await makeRequest.get(`/likes/${post.id}`, {
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      });
+      setLikes(queryLikes.data);
+      setLikesLoading((value) => !value);
+    } catch (e) {}
+  };
+
+  const handleLike = async () => {
+    const token = JSON.parse(localStorage.getItem("user")).token;
+    await makeRequest.put(
+      `/likes`,
+      { postId: post.id },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
     await getLikes();
- };
+  };
 
   useEffect(() => {
     getLikes();
-  }, [post])
+  }, [post]);
 
-   const handleLikeDelete = async () => {
-    const token = JSON.parse(localStorage.getItem('user')).token;
+  const handleLikeDelete = async () => {
+    const token = JSON.parse(localStorage.getItem("user")).token;
     await makeRequest.delete(`/likes/${post.id}`, {
-      headers:{
-         Authorization: `Bearer ${token}` 
-      
+      headers: {
+        Authorization: `Bearer ${token}`,
       },
     });
-    
+
     await getLikes();
   };
- 
-   const handleUpdate = () => {
 
-  };
- 
-  const getUser = async()=>{
+  const handleUpdate = () => {};
+
+  const getUser = async () => {
     let userQuery = await makeRequest.get(`/users/find`, {
-      headers:{
-        Authorization: `Bearer ${currentUser.token}`
+      headers: {
+        Authorization: `Bearer ${currentUser.token}`,
       },
     });
     setUser(userQuery.data[0]);
-  }
+  };
 
   useEffect(() => {
     getUser();
-  }, [])
+  }, []);
 
   return (
     <div className="post">
@@ -108,11 +103,15 @@ export default function Post({ post,handlePostDelete  }) {
             </div>
           </div>
 
-            {/* <MoreHorizIcon onClick={() => setMenuOpen(!menuOpen)} />  */}
-          {/* menuOpen && */ (post.userId === user.id|| user.isAdmin===1 )&& (
-            
-            <button onClick={()=>{handlePostDelete(post.id, post.userId)}}>Supprimer</button>
-          )} 
+          {(post.userId === user.id || user.isAdmin === 1) && (
+            <button
+              onClick={() => {
+                handlePostDelete(post.id, post.userId);
+              }}
+            >
+              Supprimer
+            </button>
+          )}
         </div>
         <div className="content">
           <p>{post.desc}</p>
@@ -123,11 +122,11 @@ export default function Post({ post,handlePostDelete  }) {
           <div className="item">
             {likesLoading ? (
               "loading..."
-             ) : likes.includes(user.id) ? ( 
+            ) : likes.includes(user.id) ? (
               <FavoriteOutlinedIcon
                 style={{ color: "red" }}
                 onClick={handleLikeDelete}
-              /> 
+              />
             ) : (
               <FavoriteBorderOutlinedIcon onClick={handleLike} />
             )}
@@ -137,16 +136,21 @@ export default function Post({ post,handlePostDelete  }) {
             <TextsmsOutlinedIcon />
             commentaires
           </div>
-            <div className="item" onClick={() => setUpdateOpen(true)}>
-            {(post.userId === user.id || user.isAdmin===1) && (
+          <div className="item" onClick={() => setUpdateOpen(true)}>
+            {(post.userId === user.id || user.isAdmin === 1) && (
               <ModeEditOutlineOutlinedIcon />
-            )} 
-          </div>   
+            )}
+          </div>
         </div>
-         {commentOpen && <Comments postId={post.id} />} 
-    
+        {commentOpen && <Comments postId={post.id} />}
       </div>
-      {updateOpen && <PostUpdate setUpdateOpen={setUpdateOpen} post={post} handleUpdate={handleUpdate} />}
+      {updateOpen && (
+        <PostUpdate
+          setUpdateOpen={setUpdateOpen}
+          post={post}
+          handleUpdate={handleUpdate}
+        />
+      )}
     </div>
   );
 }
